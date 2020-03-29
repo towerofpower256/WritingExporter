e944c1b8f3aeb56e76442d22c1241d799fc96368
David McDonald <towerofpower256@gmail.com>
2020-03-29 21:52:00 +1000

    More changes

---
+++ src/WritingExporter.Common/Data/BaseRepositoryMemory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritingExporter.Common.Data
+{
+    public class BaseMemoryRepository<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : class
+        where TKey : class
+    {
+        Dictionary<TKey, TEntity> _dict;
+        object _lock;
+
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetByID(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/WritingExporter.Common/Data/WdcStoryRepository.cs
+++ src/WritingExporter.Common/Data/WdcStoryRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WritingExporter.Common.Models;
 
 namespace WritingExporter.Common.Data
 {
-    class WdcStoryRepository
+    public class WdcStoryRepository : BaseMemoryRepository<WdcStory, string>
     {
+
     }
 }
