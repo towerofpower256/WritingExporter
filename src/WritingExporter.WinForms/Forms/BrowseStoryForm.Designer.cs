bbccfdda3982dbd7852ab7d10dd91f7ee6953f16
David McDonald <towerofpower256@gmail.com>
2020-07-13 16:50:03 +1000

    Start on chapter browser

---
+++ src/WritingExporter.WinForms/Forms/BrowseStoryForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WritingExporter.WinForms.Forms
+{
+    partial class BrowseStoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvChapters = new System.Windows.Forms.DataGridView();
+            this.dgvColumnIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvColumnTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.tblWrapper = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChapters)).BeginInit();
+            this.tblWrapper.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvChapters
+            // 
+            this.dgvChapters.AllowUserToAddRows = false;
+            this.dgvChapters.AllowUserToDeleteRows = false;
+            this.dgvChapters.AllowUserToResizeRows = false;
+            this.dgvChapters.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChapters.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvColumnIndex,
+            this.dgvColumnTitle});
+            this.dgvChapters.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvChapters.Location = new System.Drawing.Point(3, 3);
+            this.dgvChapters.MultiSelect = false;
+            this.dgvChapters.Name = "dgvChapters";
+            this.dgvChapters.ReadOnly = true;
+            this.dgvChapters.RowHeadersVisible = false;
+            this.dgvChapters.Size = new System.Drawing.Size(267, 318);
+            this.dgvChapters.TabIndex = 0;
+            // 
+            // dgvColumnIndex
+            // 
+            this.dgvColumnIndex.HeaderText = "Index";
+            this.dgvColumnIndex.Name = "dgvColumnIndex";
+            this.dgvColumnIndex.ReadOnly = true;
+            // 
+            // dgvColumnTitle
+            // 
+            this.dgvColumnTitle.HeaderText = "Title";
+            this.dgvColumnTitle.Name = "dgvColumnTitle";
+            this.dgvColumnTitle.ReadOnly = true;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInfo.Location = new System.Drawing.Point(276, 3);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(268, 318);
+            this.txtInfo.TabIndex = 1;
+            // 
+            // tblWrapper
+            // 
+            this.tblWrapper.ColumnCount = 2;
+            this.tblWrapper.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblWrapper.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblWrapper.Controls.Add(this.txtInfo, 1, 0);
+            this.tblWrapper.Controls.Add(this.dgvChapters, 0, 0);
+            this.tblWrapper.Location = new System.Drawing.Point(125, 54);
+            this.tblWrapper.Name = "tblWrapper";
+            this.tblWrapper.RowCount = 1;
+            this.tblWrapper.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblWrapper.Size = new System.Drawing.Size(547, 324);
+            this.tblWrapper.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 380);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // BrowseStoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.tblWrapper);
+            this.Name = "BrowseStoryForm";
+            this.Text = "Browse story - ";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChapters)).EndInit();
+            this.tblWrapper.ResumeLayout(false);
+            this.tblWrapper.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvChapters;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvColumnIndex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvColumnTitle;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TableLayoutPanel tblWrapper;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
---
+++ src/WritingExporter.WinForms/Forms/BrowseStoryForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WritingExporter.Common.Data.Repositories;
+
+namespace WritingExporter.WinForms.Forms
+{
+    public partial class BrowseStoryForm : Form
+    {
+        const string TITLE_START = "Browse story";
+        const string TEXT_STORY_INDEX = "-";
+        const string TEXT_EMPTY = "(empty)";
+        const string MSG_BAD_ID = "Error: invalid ID '{0}'";
+        const string MSG_UNKNOWN_STORY = "Error: couldn't find a story with the SysId '{0}'";
+        const string MSG_UNKNOWN_CHAPTER = "Error: couldn't find a chapter with the SysId '{0}'";
+        const char ID_PREFIX_DELIM = ':';
+        const char ID_PREFIX_STORY = 's';
+        const char ID_PREFIX_CHAPTER = 'c';
+
+
+        WdcStoryRepository _storyRepo;
+        WdcChapterRepository _chapterRepo;
+        string _storySysId = String.Empty;
+
+        public BrowseStoryForm(WdcStoryRepository storyRepo, WdcChapterRepository chapterRepo)
+        {
+            _storyRepo = storyRepo;
+            _chapterRepo = chapterRepo;
+
+            InitializeComponent();
+        }
+
+        private void SetStory(string storySysId)
+        {
+            _storySysId = storySysId;
+        }
+
+        private void SetTitle(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                this.Text = TITLE_START;
+            else
+                this.Text = $"{TITLE_START} - {text}";
+        }
+
+        private void ReloadChapters()
+        {
+            dgvChapters.Rows.Clear();
+
+            if (string.IsNullOrEmpty(_storySysId))
+                return;
+
+            var story = _storyRepo.GetByID(_storySysId);
+
+            if (story == null)
+            {
+                // TODO Handle missing story
+                return;
+            }
+
+            // Add the story info
+            SetTitle(story.Name);
+
+            var storyInfoRow = new DataGridViewRow();
+            storyInfoRow.Tag = $"{ID_PREFIX_STORY}{ID_PREFIX_DELIM}{_storySysId}";
+            storyInfoRow.Cells.Add(new DataGridViewTextBoxCell() { Value = TEXT_STORY_INDEX });
+            storyInfoRow.Cells.Add(new DataGridViewTextBoxCell() { Value = StringOrEmpty(story.Name) });
+
+            dgvChapters.Rows.Add(storyInfoRow);
+
+            // Add the chapters
+            var chapters = _chapterRepo.GetStoryOutline(_storySysId);
+            foreach (var c in chapters)
+            {
+                var chapterRow = new DataGridViewRow();
+
+                chapterRow.Tag = $"{ID_PREFIX_CHAPTER}{ID_PREFIX_DELIM}{c.SysId}";
+                chapterRow.Cells.Add(new DataGridViewTextBoxCell() { Value = c.Path });
+                chapterRow.Cells.Add(new DataGridViewTextBoxCell() { Value = StringOrEmpty(c.Title) });
+
+                dgvChapters.Rows.Add(chapterRow);
+            }
+        }
+
+        private void LoadInfo(string target)
+        {
+            var targetSplit = target.Split(ID_PREFIX_DELIM);
+            
+            if (targetSplit.Length != 2)
+            {
+                txtInfo.Text = string.Format(MSG_BAD_ID, target);
+            }
+
+            if (targetSplit[0] == ID_PREFIX_STORY.ToString())
+            {
+                LoadStoryInfo(targetSplit[1]);
+            }
+            else if (targetSplit[0] == ID_PREFIX_CHAPTER.ToString())
+            {
+                LoadChapterInfo(targetSplit[1]);
+            }
+            else
+            {
+                txtInfo.Text = string.Format(MSG_BAD_ID, target);
+            }
+        }
+
+        private string LoadStoryInfo(string storySysId)
+        {
+            var story = _storyRepo.GetByID(storySysId);
+
+            if (story == null)
+            {
+                return string.Format(MSG_UNKNOWN_STORY, storySysId);
+            }
+            
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Id:");
+            sb.AppendLine(story.Id);
+            sb.AppendLine();
+            sb.AppendLine("Name:");
+            sb.AppendLine(story.Name);
+            sb.AppendLine();
+            sb.AppendLine("Author:");
+            sb.AppendLine($"{story.AuthorName} ({story.AuthorUsername})");
+            sb.AppendLine();
+            sb.AppendLine("First seen:");
+            sb.AppendLine(story.FirstSeen.ToString());
+            sb.AppendLine();
+            sb.AppendLine("URL:");
+            sb.AppendLine(story.Url);
+            sb.AppendLine();
+            sb.AppendLine("Short description:");
+            sb.AppendLine(story.ShortDescription);
+            sb.AppendLine();
+            sb.AppendLine("Description:");
+            sb.AppendLine(story.Description);
+
+            return sb.ToString();
+        }
+
+        private string LoadChapterInfo(string chapterSysId)
+        {
+            var chapter = _chapterRepo.GetByID(chapterSysId);
+
+            if (chapter == null)
+            {
+                return string.Format(MSG_UNKNOWN_CHAPTER, chapterSysId);
+            }
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Path:");
+            sb.AppendLine(chapter.Path);
+            sb.AppendLine();
+            sb.AppendLine("Title:");
+            sb.AppendLine(chapter.Title);
+            sb.AppendLine();
+            sb.AppendLine("Source choice:");
+            sb.AppendLine(chapter.SourceChoiceTitle);
+            sb.AppendLine();
+            sb.AppendLine("Author:");
+            sb.AppendLine($"{chapter.AuthorName} ({chapter.AuthorUsername})");
+            sb.AppendLine();
+            sb.AppendLine("First seen:");
+            sb.AppendLine(chapter.FirstSeen.ToString());
+            sb.AppendLine();
+            sb.AppendLine("Content:");
+            sb.AppendLine(chapter.Content);
+            sb.AppendLine();
+            sb.AppendLine("Choices:");
+            foreach (var choice in chapter.Choices)
+            {
+                sb.AppendLine($"{choice.Name} ({choice.PathLink})");
+            }
+
+            return sb.ToString();
+        }
+
+        private string StringOrEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input) ? TEXT_EMPTY : input;
+        }
+    }
+}
